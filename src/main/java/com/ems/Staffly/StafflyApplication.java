1cb7f7f87fc67eb181fe9b9976968caa10377ed1
Nisith Ferdinando <nisithsandaru@gmail.com>
2025-05-24 08:54:55 +0530

    initial commit

---
+++ src/main/java/com/ems/Staffly/StafflyApplication.java
@@ -0,0 +1,13 @@
+package com.ems.Staffly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StafflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StafflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ems/Staffly/StafflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems.Staffly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StafflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
